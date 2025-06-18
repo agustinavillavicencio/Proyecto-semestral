e31f80ac38926177acd1d32e992a15efb41afd14
agustina <avillavicencio@uc.cl>
2025-06-18 12:29:05 -0400

    Update script.js

--- Entrega 5/script.js
+++ Entrega 5/script.js
@@ -1,13 +1,10 @@
-// Detectar elementos con la clase .scroll-fade
 const scrollElements = document.querySelectorAll('.scroll-fade');
 
-// Función para verificar si el elemento está en vista
 const elementInView = (el, offset = 100) => {
   const elementTop = el.getBoundingClientRect().top;
   return elementTop <= (window.innerHeight - offset);
 };
 
-// Añadir clase visible al hacer scroll
 const handleScrollAnimation = () => {
   scrollElements.forEach((el) => {
     if (elementInView(el)) {
@@ -16,6 +13,6 @@ const handleScrollAnimation = () => {
   });
 };
 
-// Ejecutar cuando se hace scroll o se carga la página
 window.addEventListener('scroll', handleScrollAnimation);
 window.addEventListener('load', handleScrollAnimation);
+
