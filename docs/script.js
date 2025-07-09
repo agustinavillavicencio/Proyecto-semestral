9d6e6a965bc0d0c07129018eb775d99a37a56cc8
agustina <avillavicencio@uc.cl>
2025-07-09 12:03:53 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -16,38 +16,6 @@ const handleScrollAnimation = () => {
 window.addEventListener('scroll', handleScrollAnimation);
 window.addEventListener('load', handleScrollAnimation);
 
-// Info para cada artista
-const infoArtistas = {
-  Beyonce: "Dua Lipa: Cantante británica-albanesa. Ícono pop moderno con estética disco y empoderamiento.",
-  Taylor: "Billie Eilish: Revolucionó el pop con su estilo minimalista y oscuro. Ganadora de múltiples Grammys.",
-  Rihanna: "Doja Cat: Fusiona pop, rap y R&B con una estética visual arriesgada y versátil.",
-  Lady: "hola",
-  Britney: "hola",
-  Ariana: "hola",
-  Adele: "hola",
-  Nicki: "hola",
-  Miley: "hola",
-  Shakira: "hola",
-  Katy: "hola",
-};
-
-
-// Mostrar popup
-document.querySelectorAll('.boton-artista').forEach(img => {
-  img.addEventListener('click', () => {
-    const clave = img.dataset.artista;
-    const contenido = infoArtistas[clave] || "Información no disponible.";
-    document.getElementById('contenido-popup').textContent = contenido;
-    document.getElementById('popup').style.display = 'block';
-  });
-});
-
-// Cerrar popup
-document.querySelector('.cerrar').addEventListener('click', () => {
-  document.getElementById('popup').style.display = 'none';
-});
-
-
 AOS.init({
   once: true,
   duration: 1000
